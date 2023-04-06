9fce39fdf1f30941e58e900392839e33cc999530
soohosuh <128343379+soohosuh@users.noreply.github.com>
2023-04-06 20:34:55 +0900

    Update FootballPlay1.java

--- javaProject/Test/src/test/frame/FootballPlay1.java
+++ javaProject/Test/src/test/frame/FootballPlay1.java
@@ -1,5 +1,72 @@
 package test.frame;
+import java.util.Scanner;
 
 public class FootballPlay1 {
+
+	public static void main(String[] args) {
+		
+//		Scanner in = new Scanner(System.in);
+//		System.out.println("나이을 입력하세요.");
+//		String a = in.nextLine();
+//		System.out.println("이름을 입력하세요.");
+//		String name = in.nextLine();
+//	
+//		
+//		
+//		
+//		for(int i=4; i<=17; i++) {
+//			System.out.println(i);
+//			if(i==14) {
+//				System.out.println("이름과 나이를 출력하세요." + (Integer.parseInt(a) + 10));
+//			}
+//		}
+		
+		
+		
+		
+		
+		
+		Scanner in = new Scanner(System.in);
+		
+		String num = in.nextLine();
+		
+		
+		String num1 = "10";
+		String num2 = "16";
+		
+		
+		System.out.println(Integer.parseInt(num)+Integer.parseInt(num2));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 
 }
