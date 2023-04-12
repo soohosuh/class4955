33eab5481c63210e605693f080dbe8a1760d1e50
soohosuh <128343379+soohosuh@users.noreply.github.com>
2023-04-12 09:07:22 +0900

    Update 20230411.sql

--- sql/20230411.sql
+++ sql/20230411.sql
@@ -72,12 +72,98 @@ select empno, ename, sal
 from emp
 where ename = 'FORD'
 ;
+-- 날짜를 비교할 때에도 작은 따옴표를 사용
 select *
 from emp
 where hiredate='81/12/03'
 ;
 
+select *
+from emp
+where hiredate>'81/12/03'
+;
 
+-- 논리 연산자 : AND, OR, NOT
+-- 10번 부서의 매니저가 누구인지 찾아보자
+select * from emp where deptno = 10 and job='MANAGER';
+select * from emp where deptno = 10 or job='MANAGER';
+select * from emp where deptno = 20 or deptno = 30 or deptno = 40;
+select * from emp where not deptno = 10;
+select * from emp where deptno <> 10;
+select * from emp where deptno != 10;
+
+select * from dept;
+
+
+-- 2000에서 3000 사이의 급여를 받는 사원
+select * from emp where sal >= 2000 and sal <= 3000;
+
+-- 범위 연산자 between A and B => A이상 B이하
+select * from emp where sal between 2000 and 3000;
+
+-- 범위 연산자의 연산은 날짜도 가능 : 날짜는 작은 따옴표로 정의
+-- 1987년에 입사한 사원 -> '87/01/01' ~ '87/12/31'
+select * from emp where hiredate between '87/01/01' and '87/12/31' ;
+
+
+-- 10번, 20번, 30번 부서에 소속한 사원의 정보를 출력
+select * from emp where deptno = 10 or deptno = 20 or deptno = 30;
+
+-- 컬럼 in(값1, 값2, ...) : or의 연산을 간단하게 처리
+-- 컬럼1 = 값1 or 컬럼 = 값2 or 컬럼 = 값3 ...
+select * from emp where deptno in(10, 20, 30);
+
+-- 패턴 검색 : 문자열의 패턴 
+-- 컬럼 LIKE '패턴'
+-- 패턴 형식 : % -> 0개이상~, '%java%' => java 문자열을 포함하는 문자열
+-- _한자리__두자리___세자리 => '__ACE' 앞 두자리는 어떤 문자가 와도 상관없고 A로 끝나는 세자리 문자열
+
+-- F로 시작하는 이름의 사원 리스트
+select * from emp where ename like 'A%';
+-- 이름에 A를 포함하는 사원 리스트 : '%A%'
+select * from emp where ename like '%A%';
+-- 이름에 N 으로 끝나는 사원의 리스트 : '%N' '%.pdf'
+select * from emp where ename like '%N';
+
+-- 사원 이름중 대번째 문자가 A인 사원 리스트
+select * from emp where ename like '__A%';
+select * from emp where ename like '__R%';
+select * from emp where ename like '%R__%';
+
+
+-- null 여부를 판단 하는 is null / is not null
+-- 커미션이 등록되지 않은 사원 리스트 => comm이 null인 사원
+select * from emp where comm is null;
+-- 커미션이 등록되어 있는 사원 리스트 => comm이 null이 아닌 사원
+select * from emp where comm is not null;
+-- 커미션을 받지 않는 사원 리스트
+select * from emp where comm=0 or comm is null;
+-- 커미션을 받는 사원의 리스트
+select * from emp where (comm=0 or comm is null);
+select * from emp where comm>0 and comm is not null;
+
+
+-- select의 출력 결과의 정력 : 오름차순 asc, 내림차순 desc
+-- select ... from ... where ... order by 컬럼이름 { [asc], [desc] }
+-- 급여순(오름차순)으로 사원 리스트 출력
+select * from emp order by sal asc;
+select * from emp order by sal;
+select * from emp order by sal desc;
+
+
+-- 문자 정력
+-- 이름 순
+select * from emp order by ename;
+select * from emp order by ename desc;
+
+-- 날짜 
+-- 입사일 빠른 순서대로 정렬 : 오름차순
+select * from emp order by hiredate;
+-- 최근 입사일 기준으로 정렬 : 내림차순
+select * from emp order by hiredate desc;
+
+-- 급여순으로 정렬
+select * from emp order by sal, ename desc;
 
 
 
